b060c7439ac8455528e19eb08f0934e35476bb8d
Ross Edlin <rossedlin@gmail.com>
2024-04-15 10:04:47 +0100

    Make a function

--- run.py
+++ run.py
@@ -8,15 +8,20 @@ def import_csv_to_array(file_path):
         return [row[0].strip() for row in reader if row]
 
 
-# Replace with your own CSV file paths
-files = ['csv/Aword.csv', 'csv/Fword.csv', 'csv/Hword.csv', 'csv/Mword.csv']
-words = [import_csv_to_array(file) for file in files]
+def attack():
+    # Replace with your own CSV file paths
+    files = ['csv/Aword.csv', 'csv/Fword.csv', 'csv/Hword.csv', 'csv/Mword.csv']
+    words = [import_csv_to_array(file) for file in files]
 
-password = 'hedgehog_finite_minus'
+    password = 'hedgehog_finite_minus'
 
-# Generate all combinations and compare with password
-for combination in product(*words):
-    potential = "_".join(combination)
-    if potential == password:
-        print('Password found:', potential)
-        break
+    # Generate all combinations and compare with password
+    for combination in product(*words):
+        potential = "_".join(combination)
+        print(potential)
+        if potential == password:
+            print('Password found:', potential)
+            break
+
+
+attack()
